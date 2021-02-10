b9d91de60b853960a359ff743542c935b040b263
samita100 <66769151+samita100@users.noreply.github.com>
2021-02-10 21:04:36 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+from dotenv import load_dotenv
+from flask import Flask
+import os
 
+load_dotenv()
+
+GREETING = os.getenv('GREETING', 'Hello')
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/hello/<name>")
+def hello(name):
+    resp = {
+      'message': GREETING + ' ' + name 
+    }
+    return resp
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=1234)
+if __name__ == '__main__': 
+  app.run(host='0.0.0.0', port=80)
